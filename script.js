0c86240e09cfbe7d4ffd18d3536c7c7d4a7e9841
Eric Sanchez <eric.q.sanchez@gmail.com>
2020-10-05 16:52:22 -0400

    render data from api.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-console.log("loading javascript")
+console.log("loading javascript");
 
-fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur").then(function (response) {
-	// The API call was successful!
-	console.log('success!', response);
-}).catch(function (err) {
-	// There was an error
-	console.warn('Something went wrong.', err);
-});
+let d = new Date();
+const span = document.createElement('span')
+span.innerHTML = "<h1>Today's date is " + d + "</h1>";
+document.body.appendChild(span);
 
+fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+  .then(function (response) {
+    // The API call was successful!
+    return response.json();
+  })
+  .then(function (data) {
+	// This is the JSON from our response
+	document.getElementById('name').innerHTML = data.name
+	console.log("success!", data);
+  })
+  .catch(function (err) {
+    // There was an error
+    console.warn("Something went wrong.", err);
+  });
